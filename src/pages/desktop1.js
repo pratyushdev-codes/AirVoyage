57569bd61358137e04c2ffaeddb6b7e8aaa4da90
pratyushdev-codes <birole.pratyush@gmail.com>
2023-06-04 19:27:42 +0530

    first commit

---
+++ src/pages/desktop1.js
@@ -0,0 +1,54 @@
+import "./desktop1.css";
+
+const Desktop1 = () => {
+  return (
+    <div className="desktop-1">
+      <div className="dc409a48815635d917aae6d912db62-parent">
+        <img
+          className="dc409a48815635d917aae6d912db62-icon"
+          alt=""
+          src="/8dc409a48815635d917aae6d912db62f-1@2x.png"
+        />
+        <img
+          className="screenshot-2023-04-07-204145-r-icon"
+          alt=""
+          src="/screenshot-20230407-204145removebgpreview-1@2x.png"
+        />
+        <div className="frame-child" />
+        <div className="all-you-flight-container">
+          <p className="all-you-flight">All you flight details on you</p>
+          <p className="all-you-flight"> finger tips!</p>
+        </div>
+        <img
+          className="e0fbb1db84cf0478615fb42b56ce89-icon"
+          alt=""
+          src="/8e0fbb1db84cf0478615fb42b56ce890-1@2x.png"
+        />
+      </div>
+      <div className="f4fd5e3a078cdf748529d2d360-1-parent">
+        <img
+          className="f4fd5e3a078cdf748529d2d360-1-icon"
+          alt=""
+          src="/135294f4fd5e3a078cdf748529d2d360-1@2x.png"
+        />
+        <img
+          className="blue-pink-linear-gradient-circ-icon"
+          alt=""
+          src="/bluepinklineargradientcirclebrochureelementvector45195239removebgpreview-1@2x.png"
+        />
+        <img
+          className="png-transparent-circle-gradien-icon"
+          alt=""
+          src="/pngtransparentcirclegradientcolor3diconthumbnailremovebgpreview-1@2x.png"
+        />
+        <div className="air-voyage">
+          <span>{`Air `}</span>
+          <span className="voyage">Voyage!</span>
+        </div>
+        <div className="your-travel-companinon">Your Travel Companinon</div>
+      </div>
+    </div>
+  );
+};
+
+export default Desktop1;
